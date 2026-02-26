80f21b307a50b7a5f5b909d00537e28a7d098517
jvictorsales <joaovictorlsales@gmail.com>
2026-02-26 18:41:26 -0300

    Exemplo de uso dos sets

---
+++ aula79.py
@@ -0,0 +1,14 @@
+# Exemplo de uso dos sets
+
+letras = set()
+
+while True:
+    letra = input('Digite uma letra: ')
+    letras.add(letra.lower())
+
+    if 'l' in letras:
+        print('Parabéns!!')
+        break
+
+    print(letras)
+    
