e45167fdae41c6e629348c2cfdda2e22e317d65b
jvictorsales <joaovictorlsales@gmail.com>
2026-03-22 21:40:24 -0300

    Classes em Python

---
+++ aula121.py
@@ -0,0 +1,29 @@
+# class - Classes são moldes para criar novos objetos
+# As classes geram novos objetos (instâncias) que
+# podem ter seus próprios atributos e métodos.
+# Os objetos gerados pela classe podem usar seus dados
+# internos para realizar várias ações.
+# Por convenção, usamos PascalCase para nomes de classes.
+
+# string = 'João'  # str
+# print(string.upper())
+# print(isinstance(string, str))
+
+class Pessoa:
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+p1 = Pessoa('João', 'Sales')
+# p1.nome = 'João'
+# p1.sobrenome = 'Sales'
+
+p2 = Pessoa('Maria', 'Silva')
+# p2.nome = 'Maria'
+# p2.sobrenome = 'Silva'
+
+print(p1.nome)
+print(p1.sobrenome)
+
+print(p2.nome)
+print(p2.sobrenome)
