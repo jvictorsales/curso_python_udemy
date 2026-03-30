f62b0d65169ab5917eedc918929995ea91029fef
jvictorsales <joaovictorlsales@gmail.com>
2026-03-30 20:42:57 -0300

    Aprendendo sobre super()

---
+++ aula139.py
@@ -0,0 +1,61 @@
+# super() é a super classe na sub classe
+# Classe principal (Pessoa)
+# -> super class, base class, parent class
+# Classes filhas (Cliente)
+# -> sub class, child class, derived class
+
+# class MinhaString(str):
+#     def upper(self):
+#         print('CHAMOU UPPER')
+#         retorno = super().upper()
+#         # retorno = super(MinhaString, self).upper()
+#         print('DEPOIS DO UPPER')
+#         return retorno
+
+# string = MinhaString('João')
+# print(string.upper())
+
+class A(object):
+    atributo_a = 'valor a'
+
+    def __init__(self, atributo):
+        self.atributo = atributo
+
+    def metodo(self):
+        print('A')
+
+class B(A):
+    atributo_b = 'valor b'
+    
+    def __init__(self, atributo, outra_coisa):
+        super().__init__(atributo)
+        self.outra_coisa = outra_coisa
+
+    def metodo(self):
+        print('B')
+
+class C(B):
+    atributo_c = 'valor c'
+
+    def __init__(self, *args, **kwargs):
+        # print('EI, BURLEI O SISTEMA.')
+        super().__init__(*args, **kwargs)
+
+    def metodo(self):
+        super().metodo()  # B
+        # super(B, self).metodo()  # A
+        # super(A, self).metodo()  # object
+        # A.metodo(self)
+        # B.metodo(self)
+        print('C')
+
+# print(C.mro())
+# print(B.mro())
+# print(A.mro())
+c = C('Atributo', 'Qualquer')
+# print(c.atributo_a)
+# print(c.atributo_b)
+# print(c.atributo_c)
+# print(c.atributo)
+# print(c.outra_coisa)
+c.metodo()
