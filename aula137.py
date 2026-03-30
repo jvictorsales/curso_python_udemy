7a51dd692cd93ba3e95e4313a2c6b22e53aeb888
jvictorsales <joaovictorlsales@gmail.com>
2026-03-30 00:45:35 -0300

    Exercício com classes v2

--- aula137.py
+++ aula137.py
@@ -11,6 +11,12 @@
 class Fabricante:
     def __init__(self, nome):
         self.nome = nome
+        self.carros = []
+
+    def listar_carros(self):
+        for carro in self.carros:
+            print(f'- {carro.nome}')
+        print()
 
 class Motor:
     def __init__(self, nome):
@@ -27,23 +33,27 @@ class Carro:
         return self._motor
     
     @motor.setter
-    def motor(self, nome):
-        self._motor = nome
+    def motor(self, motor):
+        self._motor = motor
     
     @property
     def fabricante(self):
         return self._fabricante
     
     @fabricante.setter
-    def fabricante(self, nome):
-        self._fabricante = nome
-
+    def fabricante(self, fabricante):
+        self._fabricante = fabricante
+        fabricante.carros.append(self)
 
 fiat = Fabricante('Fiat')
 motor_1_0 = Motor('1.0')
 fiat_uno = Carro('Uno')
 fiat_uno.motor = motor_1_0
 fiat_uno.fabricante = fiat
+
+siena = Carro('Siena')
+siena.motor = motor_1_0
+siena.fabricante = fiat
 
 chevrolet = Fabricante('Chevrolet')
 motor_2_0 = Motor('2.0')
@@ -59,3 +69,14 @@ focus.fabricante = ford
 print(opala.nome, opala.motor.nome, opala.fabricante.nome)
 print(fiat_uno.nome, fiat_uno.motor.nome, fiat_uno.fabricante.nome)
 print(focus.nome, focus.motor.nome, focus.fabricante.nome)
+
+print()
+
+print('Carros Fiat')
+fiat.listar_carros()
+
+print('Carros Chevrolet')
+chevrolet.listar_carros()
+
+print('Carros Ford')
+ford.listar_carros()
