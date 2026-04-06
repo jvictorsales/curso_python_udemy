f68721b4cb5cbed4a4327847f05289d6a551b109
jvictorsales <joaovictorlsales@gmail.com>
2026-04-06 14:58:27 -0300

    Polimorfismo em Python Orientado a Objetos

---
+++ aula144.py
@@ -0,0 +1,57 @@
+# Polimorfismo em Python Orientado a Objetos
+# Polimorfismo é o princípio que permite que
+# classes deridavas de uma mesma superclasse
+# tenham métodos iguais (com mesma assinatura)
+# mas comportamentos diferentes.
+# Assinatura do método = Mesmo nome e quantidade
+# de parâmetros (retorno não faz parte da assinatura)
+# Opinião + princípios que contam:
+# Assinatura do método: nome, parâmetros e retorno iguais
+# SO"L"ID
+# Princípio da substituição de liskov
+# Objetos de uma superclasse devem ser substituíveis
+# por objetos de uma subclasse sem quebrar a aplicação.
+# Sobrecarga de métodos (overload)  🐍 = ❌
+# Sobreposição de métodos (override) 🐍 = ✅
+
+from abc import ABC, abstractmethod
+
+class Notificacao(ABC):
+    def __init__(self, mensagem) -> None:
+        self.mensagem = mensagem
+
+    @abstractmethod
+    def enviar(self) -> bool: ...
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self) -> bool:
+        print('E-mail: enviando -', self.mensagem)
+        return True
+        
+class NotificacaoSMS(Notificacao):
+    def enviar(self) -> bool:
+        print('SMS: enviando -', self.mensagem)
+        return True
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+
+    if notificacao_enviada:
+        print('Notificação enviada')
+    else:
+        print('Notificação NÃO enviada')
+
+# n = NotificacaoSMS('testando notificação')
+# n.enviar()
+# n = NotificacaoEmail('testando notificação')
+# n.enviar()
+
+notificacao_email = NotificacaoEmail('testando email')
+notificar(notificacao_email)
+
+print()
+
+notificacao_sms = NotificacaoSMS('testando sms')
+notificar(notificacao_sms)
+
+print()
