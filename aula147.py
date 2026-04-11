cc7b5df0b0293ae072ff9640c66be0239999ffa1
jvictorsales <joaovictorlsales@gmail.com>
2026-04-11 17:29:14 -0300

    Python Special Methods, Magic Methods ou Dunder Methods

---
+++ aula147.py
@@ -0,0 +1,65 @@
+# Python Special Methods, Magic Methods ou Dunder Methods
+# Dunder = Double Underscore = __dunder__
+# Antigo e útil: https://rszalski.github.io/magicmethods
+# https://docs.python.org/3/reference/datamodel.html#specialnames
+# __lt__(self,other) - self < other
+# __le__(self,other) - self <= other
+# __gt__(self,other) - self > other
+# __ge__(self,other) - self >= other
+# __eq__(self,other) - self == other
+# __ne__(self,other) - self != other
+# __add__(self,other) - self + other
+# __sub__(self,other) - self - other
+# __mul__(self,other) - self * other
+# __truediv__(self,other) - self / other
+# __neg__(self) - -self
+# __str__(self) - str
+# __repr__(self) - str
+
+
+class Ponto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    # def __str__(self):
+    #     return f'({self.x}, {self.y})'
+
+    def __repr__(self):
+        # class_name = self.__class__.__name__
+        class_name = type(self).__name__
+        return f'{class_name}(x={self.x!r}, y={self.y!r})'
+
+    def __add__(self, other):
+        if not isinstance(other, Ponto):
+            return NotImplemented
+        
+        return Ponto(self.x + other.x, self.y + other.y)
+
+    def __gt__(self, other):
+        resultado_self = self.x + self.y
+        resultado_other = other.x + other.y
+        return resultado_self > resultado_other
+            
+
+class Carrinho:
+    ...
+
+if __name__ == '__main__':
+    # p1 = Ponto(1, 2)
+    # p2 = Ponto(978, 876)
+
+    # print(p1)
+    # print(p2)
+
+    # print(repr(p1))
+    # print(repr(p2))
+    # print(f'{p1!r}')
+    # print(f'{p2!r}')
+
+    p1 = Ponto(4, 2)  # 6
+    p2 = Ponto(6, 4)  # 10
+    p3 = p1 + p2
+    print(p3)
+    print('p1 é maior que p2', p1 > p2)
+    print('p2 é maior que p1', p2 > p1)
